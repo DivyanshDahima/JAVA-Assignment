c886f4da3dc4058a1ed877fbbdd90b5c009c50a9
DivyanshDahima <divyanshdahima7@gmail.com>
2024-11-21 02:03:30 +0530

    Add files via upload

---
+++ Q8.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Q8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number to be checked: ");
+        int number = scanner.nextInt();
+
+        System.out.print("Enter the divisor: ");
+        int divisor = scanner.nextInt();
+
+        if (divisor == 0) {
+            System.out.println("Division by zero is not possible.");
+        } else if (number % divisor == 0) {
+            System.out.println(number + " is divisible by " + divisor + ".");
+        } else {
+            System.out.println(number + " is not divisible by " + divisor + ".");
+        }
+
+        scanner.close();
+    }
+}
