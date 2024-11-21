cd02a0d65afe2d30a440361f96c3fac743c423d1
DivyanshDahima <divyanshdahima7@gmail.com>
2024-11-22 01:22:24 +0530

    Add files via upload

---
+++ Q22.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        double[] numbers = new double[n];
+        System.out.println("Enter the numbers:");
+        for (int i = 0; i < n; i++) {
+            numbers[i] = sc.nextDouble();
+        }
+
+        Arrays.sort(numbers);
+
+        double median;
+        if (n % 2 == 0) {
+            median = (numbers[n / 2 - 1] + numbers[n / 2]) / 2.0;
+        } else {
+            median = numbers[n / 2];
+        }
+
+        System.out.println("The median is: " + median);
+
+        sc.close();
+    }
+}
