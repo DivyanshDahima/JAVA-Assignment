84aab8cd94d3de632cfa80c81164fd3216b7f858
DivyanshDahima <divyanshdahima7@gmail.com>
2024-11-22 01:33:37 +0530

    Add files via upload

---
+++ Q23A.java
@@ -0,0 +1,14 @@
+public class Q23A {
+    public static void main(String[] args) {
+        int number = 1;
+        int rows = 3;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print(number + " ");
+                number++;
+            }
+            System.out.println();
+        }
+    }
+}
