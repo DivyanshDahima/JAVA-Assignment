67b21e0727f0b03617b21d3f88e0054af8338958
DivyanshDahima <divyanshdahima7@gmail.com>
2024-11-22 01:04:04 +0530

    Add files via upload

---
+++ Q20.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Q20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+
+        sc.close();
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
